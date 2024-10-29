1352a2560652b668a9086155e05b4d75846f8929
Laura Viglioni <laura@vio.com>
2024-10-29 19:35:21 -0300

    added title

--- static/js/back-button.js
+++ static/js/back-button.js
@@ -6,6 +6,7 @@ export const addBackButton = () => {
   const btn = document.createElement("BUTTON")
   btn.addEventListener("click", back)
   btn.id = "back-btn"
+  btn.title = "voltar"
 
   const img = document.createElement("img")
   img.src = "./static/pics/back.svg"
